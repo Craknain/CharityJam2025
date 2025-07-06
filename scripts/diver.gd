298cdc214a28c20f0c9c45cfe645ec5ea3293bc5
Richard Galdeano <richard.galdeano@etu.univ-grenoble-alpes.fr>
2025-07-06 02:32:23 +0200

    Method for diver to lose oxygen

--- scripts/diver.gd
+++ scripts/diver.gd
@@ -2,12 +2,14 @@ extends CharacterBody2D
 
 @onready var oxygen_timer: Timer = $Camera2D/OxygenTimer
 
+var MAX_OXYGEN := 30
 
-const SPEED = 300.0
+const SPEED := 300.0
+
+func _ready():
+	oxygen_timer.wait_time = MAX_OXYGEN
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
 	velocity = direction * SPEED
 	move_and_slide()
@@ -17,5 +19,11 @@ func _on_oxygen_timer_timeout() -> void:
 	print("Dead") # TODO : replace with go to scene Game Over
 	
 func refill_oxygen(amount: int):
-	oxygen_timer.start(oxygen_timer.time_left + amount)
+	oxygen_timer.start(min(oxygen_timer.time_left + amount, MAX_OXYGEN))
+	
+func lose_oxygen(amount: int):
+	if oxygen_timer.time_left - amount < 0:
+		print("Dead") # TODO: Go to game over page
+	oxygen_timer.start(oxygen_timer.time_left - amount)
+	
 	
