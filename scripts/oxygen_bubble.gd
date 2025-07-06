bfd835c69366d4e0778f2c3f99ad1a49606b8442
Richard Galdeano <richard.galdeano@etu.univ-grenoble-alpes.fr>
2025-07-07 00:51:18 +0200

    Modified bubble sprite and added timetolive timer

--- scripts/oxygen_bubble.gd
+++ scripts/oxygen_bubble.gd
@@ -1,5 +1,6 @@
 extends AnimatedSprite2D
 
+@onready var time_to_live_timer: Timer = $TimeToLiveTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +16,7 @@ func _on_collect_area_zone_body_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
 		body.refill_oxygen(5)
 		queue_free()
+
+
+func _on_time_to_live_timer_timeout() -> void:
+	queue_free()
