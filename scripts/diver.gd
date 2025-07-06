27efb2f97c6bd8fd457fe3cb89159cad5d4e343d
Richard Galdeano <richard.galdeano@etu.univ-grenoble-alpes.fr>
2025-07-07 00:39:38 +0200

    Diver swimming and slower

--- scripts/diver.gd
+++ scripts/diver.gd
@@ -3,16 +3,22 @@ extends CharacterBody2D
 @onready var oxygen_timer: Timer = $Camera2D/OxygenTimer
 @onready var i_frame: AnimationPlayer = $iFrame
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var MAX_OXYGEN := 30
 
-const SPEED := 300.0
+const SPEED := 150.0
 
 func _ready():
 	oxygen_timer.wait_time = MAX_OXYGEN
 
 func _physics_process(delta: float) -> void:
 	var direction := Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
+	rotation = global_position.angle_to_point(direction + global_position)
+	if rotation > PI/2 or rotation < -PI/2:
+		animated_sprite_2d.flip_v = true
+	else:
+		animated_sprite_2d.flip_v = false
 	velocity = direction * SPEED
 	move_and_slide()
 
