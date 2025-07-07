45a045c632ea5e5ef83bb2c697983d98ca364778
Richard Galdeano <richard.galdeano@etu.univ-grenoble-alpes.fr>
2025-07-07 02:56:53 +0200

    Bubble oxygen timer autostart disabled

--- scripts/oxygen_bubble.gd
+++ scripts/oxygen_bubble.gd
@@ -2,15 +2,6 @@ extends AnimatedSprite2D
 
 @onready var time_to_live_timer: Timer = $TimeToLiveTimer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 
 func _on_collect_area_zone_body_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
