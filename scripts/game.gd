7bdc6d9109aa1697c928b23fa4f395f8019d3e6e
Richard Galdeano <richard.galdeano@etu.univ-grenoble-alpes.fr>
2025-07-07 02:57:51 +0200

    Added victory mechanic

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,15 +1,19 @@
 extends Node
 
+@onready var item_count_label: Label = $CharacterBody2D/CanvasLayer/ItemCountLabel
 
 
 @onready var pause: Control = $CharacterBody2D/Camera2D/pause
 
 
 
-func _ready():
-	pass
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"): # "ui_cancel" is Esc by default
 		pause.show()
 		get_tree().paused = true
+		
+func _process(delta: float) -> void:
+	if get_tree().get_nodes_in_group('items').is_empty():
+		get_tree().change_scene_to_file("res://scenes/victory_screen.tscn")
+	item_count_label.text = "Remaining items : %d" % get_tree().get_nodes_in_group('items').size()
